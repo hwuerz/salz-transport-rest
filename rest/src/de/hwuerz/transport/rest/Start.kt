f05fad7798a59626d4f58e176603a2aa6754cba2
Hendrik Wuerz <h.m.wuerz@web.de>
2018-03-25 15:36:18 +0200

    - added near request support

--- rest/src/de/hwuerz/transport/rest/Start.kt
+++ rest/src/de/hwuerz/transport/rest/Start.kt
@@ -5,6 +5,8 @@ import com.amazonaws.services.lambda.runtime.RequestHandler
 import com.google.gson.JsonObject
 import com.google.gson.JsonParser
 import de.hwuerz.transport.rest.pteConnector.Lookup
+import de.schildbach.pte.dto.Location
+import de.schildbach.pte.dto.LocationType
 import de.schildbach.pte.dto.Product
 
 
@@ -19,14 +21,18 @@ class Start : RequestHandler<Any, Start.ResponseClass> {
     interface ResponseData
     data class ResponseDataEmpty(val data: String? = null) : ResponseData
     data class ResponseDataDepartures(val departures: List<ResponseDataDeparture>) : ResponseData
-
     data class ResponseDataDeparture(val startStation: String?,
                                      val plannedTime: Long?,
                                      val predictedTime: Long?,
                                      val product: Product?,
                                      val label: String?,
                                      val destinationPlace: String?,
-                                     val destinationName: String?) : ResponseData
+                                     val destinationName: String?)
+
+    data class ResponseDataNears(val near: List<ResponseDataNear>) : ResponseData
+    data class ResponseDataNear(val id: String,
+                                val destinationPlace: String?,
+                                val destinationName: String)
 
     override fun handleRequest(input: Any, context: Context): ResponseClass  {
 
@@ -56,6 +62,36 @@ class Start : RequestHandler<Any, Start.ResponseClass> {
                             publicTransport.second.line.label, // 0
                             publicTransport.second.destination?.place, // Frankfurt
                             publicTransport.second.destination?.name) // Hauptbahnhof
+                    })
+
+            return ResponseClass(ResponseMeta(), responseData)
+
+        } else if (jsonObject.has("near")) {
+            val near = jsonObject.getAsJsonObject("near")
+            if (!near.has("lat")) {
+                return ResponseClass(
+                        ResponseMeta("Error", 422, "The parameter 'lat' of near is missing"),
+                        ResponseDataEmpty())
+            }
+            if (!near.has("long")) {
+                return ResponseClass(
+                        ResponseMeta("Error", 422, "The parameter 'long' of near is missing"),
+                        ResponseDataEmpty())
+            }
+
+            val lat = near.getAsJsonPrimitive("lat").asDouble
+            val long = near.getAsJsonPrimitive("long").asDouble
+
+            val responseData = ResponseDataNears(lookup
+                    .nearby(Location.coord((lat * 1E6).toInt(), (long * 1E6).toInt()))
+                    .locations
+                    .filter { location -> location.type == LocationType.STATION }
+                    .filter { location -> location.hasId() }
+                    .filter { location -> location.hasName() }
+                    .map {location -> ResponseDataNear(
+                            location.id!!,
+                            location.place,
+                            location.name!!)
                     })
 
             return ResponseClass(ResponseMeta(), responseData)
